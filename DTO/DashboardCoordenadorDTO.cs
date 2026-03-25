1327909ef254ccf10b383e666243fad2422c9eb9
gustavomarmo <gsmarmo@gmail.com>
2026-03-25 01:03:29 -0300

    fix: retorno de totalTurmas vindo da SP de KPIs do Coordenador

--- DTO/DashboardCoordenadorDTO.cs
+++ DTO/DashboardCoordenadorDTO.cs
@@ -12,6 +12,7 @@
     {
         public int totalAlunos { get; set; }
         public int totalProfessores { get; set; }
+        public int totalTurmas { get; set; }
         public decimal mediaGeralEscola { get; set; }
     }
 
