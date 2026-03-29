0efbafca0ed4dea6680389a9f1c379909d844d66
gustavomarmo <gsmarmo@gmail.com>
2026-03-29 20:45:34 -0300

    fix: removendo texto do Service

--- Service/TurmaService.cs
+++ Service/TurmaService.cs
@@ -6,7 +6,7 @@ namespace edu_connect_backend.Service
 {
     public class TurmaService
     {
-        private readonly TurmaRepository turmaRepository;
+        private readonly TurmaRepository turma;
 
         public TurmaService(TurmaRepository turmaRepository)
         {
